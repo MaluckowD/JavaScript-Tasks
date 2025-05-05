fd8634ad0daacdd9b6cb9757c313b05545e5ae23
Mitya <dim.maluckow2017@yandex.ru>
2025-05-05 20:13:21 +0300

    solved more problems

---
+++ Основы JavaScript/2.13/index.js
@@ -0,0 +1,12 @@
+
+for (let i = 2; i < 11; i++){
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+let number = null
+
+while (number <= 100 || number === null){
+    number = +prompt("Введите число")
+}
