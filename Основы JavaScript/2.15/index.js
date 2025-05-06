f4f3ba3479e003f138e5e0fd73b1630f82b1153e
Mitya <dim.maluckow2017@yandex.ru>
2025-05-06 10:36:12 +0300

    solved all problems on javascript begginers

--- Основы JavaScript/2.15/index.js
+++ Основы JavaScript/2.15/index.js
@@ -1,55 +1,23 @@
 
 // task 1
-switch (browser) {
-    case 'Edge':
-      alert( "You've got the Edge!" );
-      break;
-  
-    case 'Chrome':
-    case 'Firefox':
-    case 'Safari':
-    case 'Opera':
-      alert( 'Okay we support these browsers too' );
-      break;
-  
-    default:
-      alert( 'We hope that this page looks ok!' );
+
+function checkAge(age) {
+
+  return age > 18 ? true : confirm('Родители разрешили?')
+  return (age > 18) || confirm('Родители разрешили?');
 }
 
 
-let browser = prompt()
-alert( browser === 'Edge' ? "You've got the Edge!" : (browser === 'Chrome' ||  browser ==='Firefox' || 
-    browser ==='Safari' || browser === 'Opera') ? 'Okay we support these browsers too' : 'We hope that this page looks ok!'
-)
-
-// task 2
-
-const number = +prompt('Введите число между 0 и 3', '');
-
-if (number === 0) {
-  alert('Вы ввели число 0');
+function min(a, b) {
+  return a > b ? b : a
 }
 
-if (number === 1) {
-  alert('Вы ввели число 1');
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
 }
-
-if (number === 2 || number === 3) {
-  alert('Вы ввели число 2, а может и 3');
-}
-
-switch(number){
-    case 0:
-        alert('Вы ввели число 0');
-        break;
-    case 1:
-        alert('Вы ввели число 1');
-        break;
-    case 2:
-    case 3:
-        alert('Вы ввели число 1');
-        break;
-}
-
-
-
